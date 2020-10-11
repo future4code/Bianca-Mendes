ed517d7538643f76793d7911ab80fd6602c8b9fa
b-bianca <you@example.com>
2020-10-11 16:26:18 -0300

    ex. logica programacao 1

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,6 +1,4 @@
-//------------------------- EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO--------------------------------
-
-//o que faz, como faz, o que será impresso
+//------------------------- EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO -------------------------------
 
 //1. A função converte um valor de dolar para real. A constante cotação, recebe através do usuário a cotação do dia, e a função recebe como parametro o valor a ser convertido. O return dentro da função fará a conta necessária pra conversão, e assim que o console.log for chamado, o que será impresso é o valor do return. Resultado: console.log(meuDinheiro) -> R$500 (usando 5.00 de cotação).
 
@@ -8,4 +6,22 @@
 
 //3. O código mostra um array com numeros, e outros dois arrays vazios. Logo depois aparece o laço for...of. O laço será responsável por ler todos os elementos do array cheio. Dentro do laço a condicional if irá separar os numeros pares e coloca-los dentro do primeiro array vazio, para isso utilizará a ferramenta push; já os outros numeros serão enviados para o segundo array vazio, também utilizando a ferramenta push.O primeiro console irá imprimir a quantidade total de elementos do array principal, o segundo console imprimirá o array de numeros pares e o terceiro console imprimirá os numeros restantes. Resultado: console1: 14, console2: 6, console3: 8.
 
-//4. Na primeira linha desse código tem um array atribuido para uma constante. Logo abaixo, duas variáveis let. Depois um laço for...of que tem como parametro o array. Dentro do laço a condicional if tem como parametro: elemento do array < primeira let(infinito), caso seja positivo, o valor da let será igual o valor do elemento do array. Depois de sair dessa if, o mesmo elemento entra num segundo if, que tem como parametro( elemento do array > segunda let(0)), caso seja positivo, o valor da segunda let será igual o valor do elemento do array. Após o fechamento do laço for, existe dois consoles, cada um referente a uma let do inicio. Resultado: o primeiro console irá imprimir: -10, o segundo console irá imprimir 1590. 
+//4. Na primeira linha desse código tem um array atribuido para uma constante. Logo abaixo, duas variáveis let. Depois um laço for...of que tem como parametro o array. Dentro do laço a condicional if tem como parametro: elemento do array < primeira let(infinito), caso seja positivo, o valor da let será igual o valor do elemento do array. Depois de sair dessa if, o mesmo elemento entra num segundo if, que tem como parametro( elemento do array > segunda let(0)), caso seja positivo, o valor da segunda let será igual o valor do elemento do array. Após o fechamento do laço for, existe dois consoles, cada um referente a uma let do inicio. Resultado: o primeiro console irá imprimir: -10, o segundo console irá imprimir 1590.
+
+
+//--------------------------- EXERCICIOS DE LÓGICA DE PROGRAMAÇÃO -------------------------------
+
+//1.As três maneiras de se percorrer/iterar uma lista são: while. for, for...of. 
+//Exemplo:
+/*
+const cores = ["preto", "azul", "amarelo", "roxo", "vermelho"]
+
+function arrayCores(array) {
+for (let cor of cores) {
+    console.log(cor)
+}
+}
+arrayCores(cores)
+*/
+
+
