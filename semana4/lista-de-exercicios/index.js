13721e19b29a032f0d98858e101bd7f20dde3da5
b-bianca <you@example.com>
2020-10-12 12:41:18 -0300

    ex. funcoes de array 5

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -398,3 +398,27 @@ const emailConsultas = consultas.map((pessoa, index, array) => {
 
 console.log(emailConsultas)
 */
+
+
+//5.
+
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.forEach((cliente, index, array) => {
+    let soma = 0
+    for (let compra of cliente.compras) {
+        soma += compra
+    }
+    
+    cliente.saldoTotal -= soma
+    
+})
+
+console.log(contas)
