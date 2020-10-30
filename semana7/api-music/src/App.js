be53d2eb23912a1a587ff44e02c23f49aed06fd9
b-bianca <you@example.com>
2020-10-29 22:55:18 -0300

    finalizacao api-space-semcss

--- semana7/api-music/src/App.js
+++ semana7/api-music/src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import AllEvents from "./components/AllEvents"
+import styled from "styled-components"
+
 
 export default class App extends React.Component {
 
--- semana7/api-music/src/components/AllEvents.js
+++ semana7/api-music/src/components/AllEvents.js
@@ -1,5 +1,27 @@
 import React from "react"
 import axios from "axios"
+import styled from "styled-components"
+
+
+/*
+const Title = styled.h2`
+  text-align: center;
+  font-family: "Arial Black", Gadget, sans-serif;
+  font-size: 30px;
+` 
+*/
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  //background-image: linear-gradient(to bottom, #462eb7, #3b33b2, #2f36ad, #2439a8, #193ba2, #1c3792, #1d3283, #1e2e74, #1f245a, #1b1b41, #15122b, #0b0516);
+  //height: 100%;
+`
+const Selector = styled.select`
+  margin: 20px;
+  width: 20%;
+`
 
 
 const apiUrl = "https://api.spacexdata.com/v3/history"
@@ -7,7 +29,10 @@ const apiUrl = "https://api.spacexdata.com/v3/history"
 export default class AllEvents extends React.Component {
     state = {
         histEvents: [], 
-        eventInfo: ""
+        eventDetail: "",
+        eventArticle:"",
+        aboutEvent: false,
+        //happyyyyyyArray:[]
         
     }
 
@@ -17,41 +42,72 @@ export default class AllEvents extends React.Component {
 
 
     fetchEvent = () => {
+
         axios.get(apiUrl)
         .then((response) => {
+            //const sadArray = response.data.map((hungry) => {return hungry.links.article})
+            //console.log(sadArray)
+            //this.setState({happyyyyyyArray: sadArray})
             //console.log("teste", response)
             this.setState({histEvents: response.data})
         })
     }
 
     onChangeSelect = (event) => {
+        this.viewAbout()  
+        
         //console.log("selecionou", event.target.value)
-        axios.get(`https://api.spacexdata.com/v3/history/{${event.target.value}}`)
+        axios.get(`https://api.spacexdata.com/v3/history/${event.target.value}`)
         .then((response) => {
-            this.setState({
-                eventInfo: response.details,
-                eventInfo: response.links.article,
-                eventInfo: response.links.wikipedia
+            
+           //console.log(response.data.links.article,"teste")
+
+           this.setState({
+           eventDetail:response.data.details,
+           eventArticle: response.data.links.article
             })
-           console.log(response,"teste")
         })
     }
 
+    viewAbout = () => {
+      
+        this.setState({aboutEvent: true})
+    }
+    
     render ()  {
+        //console.log(this.state.happyyyyyyArray)
         //console.log(this.state.histEvents)
         const eventList = this.state.histEvents.map((occurrence) => {
-            return <option key={occurrence.id}>{occurrence.title}</option>
+            return <option value={occurrence.id} key={occurrence.id}>{occurrence.title}</option>
         })
 
+        const renderDetails = this.state.aboutEvent ? 
+        <div>
+            <h2>Detalhes:</h2>
+                <p>{this.state.eventDetail}</p>
+            <h2>Artigos:</h2>   
+                <p>{this.state.eventArticle }</p>
+        </div> : <></>
+
+
         return (
-            <div>
-                <h2>Escolha um evento histórico</h2>
-                <select onChange={this.onChangeSelect}>
-                    {eventList}
-                    {this.state.eventInfo}
-                </select>
-        
-            </div>
+            
+            <Wrapper>
+                <h1>Escolha um evento histórico</h1>
+                <Selector onChange={this.onChangeSelect}>
+                   
+                   {eventList}
+                   
+                </Selector>
+                {renderDetails}
+            </Wrapper>
         )
     }
 }
+
+//<div>
+//<h2>Detalhes:</h2>
+//    <p>{this.state.eventDetail}</p>
+//<h2>Artigos:</h2>   
+//    <p>{this.state.eventArticle }</p>
+//</div>
