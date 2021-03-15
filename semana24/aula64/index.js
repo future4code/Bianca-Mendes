ad95f47697ebbf6cf9dc701250f9aaea3bdab682
b-bianca <you@example.com>
2021-03-15 17:49:03 -0300

    inicio ex2

--- semana24/aula64/index.js
+++ semana24/aula64/index.js
@@ -1,31 +1,20 @@
 // --------------- EXERCICIO 01 ---------------
 // Considere que a gente só possa fazer três operações com string: adicionar um caractere a ele, remover um caractere dele ou substituir um caractere por outro. Dizemos que uma string é 'one edit' de outra se ela for o resultado da original a partir de UMA SÓ dessas operações. Escreva um método que determine se uma string é  'one edit' de outra.
-// `"banan"` é 'one edit' de `"banana"` (remoção de um caracter)
-// `"bananak"` é 'one edit' de `"banana"` (adição de um caracter)
-// `"panana"` é 'one edit' de `"banana"` (troca de um caracter)
-// `"bananaaa"` **não** é 'one edit' de `"banana"` (adição de dois caracteres)
-function isOneEdit(stringA, stringB) {
-    //pra adicionar somente uma letra no B, se adicionar mais de uma, não é one edit
-    if (Math.abs(stringB.length - stringA.length) > 1)
-        return false;
-    //retirar uma letra do B
-    if (stringA.length > stringB.length)
-        return stringA.includes(stringB);
-    //adicionar uma letra no B
-    if (stringB.length > stringA.length)
-        return stringB.includes(stringA);
-    //alterar a última letra
-    let caracter = 0;
-    for (let i = 0; i < stringA.length; i++) {
-        if (stringA[i] !== stringB[i]) {
-            caracter = +1;
+const letterCounter = (input) => {
+    let hashInput = {};
+    //se o caractere for unico adiciona uma vez no {}, se for repetido,só aumenta o numero
+    for (let char of input) {
+        if (hashInput[char]) {
+            hashInput[char] += 1;
+        }
+        else {
+            hashInput[char] = 1;
         }
     }
-    if (caracter === 1) {
-        return true;
+    if (hashInput.length > input.length) {
+        return input;
     }
-    else {
-        false;
-    }
-}
-console.log(isOneEdit("banana", "banan"));
+    console.log(hashInput, "hash");
+    console.log(input, "input");
+};
+console.log(letterCounter('amora'));
--- semana24/aula64/index.ts
+++ semana24/aula64/index.ts
@@ -1,39 +1,69 @@
 // --------------- EXERCICIO 01 ---------------
 // Considere que a gente só possa fazer três operações com string: adicionar um caractere a ele, remover um caractere dele ou substituir um caractere por outro. Dizemos que uma string é 'one edit' de outra se ela for o resultado da original a partir de UMA SÓ dessas operações. Escreva um método que determine se uma string é  'one edit' de outra.
 
-// `"banan"` é 'one edit' de `"banana"` (remoção de um caracter)
+// function isOneEdit (stringA: string, stringB: string): boolean {
 
-// `"bananak"` é 'one edit' de `"banana"` (adição de um caracter)
+//     //pra adicionar somente uma letra no B, se adicionar mais de uma, não é one edit
+//     if(Math.abs(stringB.length - stringA.length) > 1) return false
 
-// `"panana"` é 'one edit' de `"banana"` (troca de um caracter)
+//     //retirar uma letra do B
+//     if(stringA.length > stringB.length) return stringA.includes(stringB)
 
-// `"bananaaa"` **não** é 'one edit' de `"banana"` (adição de dois caracteres)
+//     //adicionar uma letra no B
+//     if(stringB.length > stringA.length) return stringB.includes(stringA)
 
-function isOneEdit (stringA: string, stringB: string): boolean {
-
-    //pra adicionar somente uma letra no B, se adicionar mais de uma, não é one edit
-    if(Math.abs(stringB.length - stringA.length) > 1) return false
-
-    //retirar uma letra do B
-    if(stringA.length > stringB.length) return stringA.includes(stringB)
-
-    //adicionar uma letra no B
-    if(stringB.length > stringA.length) return stringB.includes(stringA)
-
-    //alterar a última letra
-    let caracter: number = 0
-    for(let i=0; i < stringA.length; i++) {
-        if(stringA[i] !== stringB[i]){
-            caracter =+ 1
-        } 
-    }
+//     //alterar a última letra
+//     let caracter: number = 0 //numero de letras diferentes
+//     for(let i=0; i < stringA.length; i++) {
+//         if(stringA[i] !== stringB[i]){
+//             caracter =+ 1
+//         } 
+//     }
     
-            if(caracter === 1) {
-                return true 
-            } else {
-                false
-            }
+//             if(caracter === 1) {
+//                 return true 
+//             } else {
+//                 false
+//             }
 
 
+// }
+// console.log(isOneEdit("banana", "banan"))
+
+// --------------- EXERCICIO 02 ---------------
+// Implemente um método que performe uma compressão de string usando a contagem dos caracteres repetidos em sequência. Caso o resultado final tamanho maior do que a string inicial, seu programa deve retornar a string inicial
+
+// Para o input `aabbb` o resultado deve ser `a2b3`
+
+// Para o input `aabcccccaaa` o resultado deve ser `a2b1c5a3`
+
+// Para o input `accurate` o resultado deve ser `accurate` (já que inicialmente o código retornaria `a1c2u1r1a1t1e1` que possui o tamanho maior do que `accurate`)
+
+// Para o input `escola` o resultado deve ser `escola` (já que inicialmente o código retornaria `e1s1c1o1l1a1` que possui o tamanho maior do que `escola`)
+
+// Para o input `accuraaaaaaaaaate`, o resultado deve ser `a1c2u1r1a10t1e1`
+
+interface HashCountCharacters { // index signature ou indexação de propriedades
+    [index: string]: number
 }
-console.log(isOneEdit("banana", "banan"))
+
+const letterCounter = (input: string): any => {
+    let hashInput: HashCountCharacters = {}
+       
+    //se o caractere for unico adiciona uma vez no {}, se for repetido,só aumenta o numero
+    for (let char of input) {
+        if (hashInput[char]) {
+            hashInput[char] += 1
+        } else {
+            hashInput[char] = 1
+        }
+    }
+
+    if (hashInput.length > input.length) {
+        return input
+    }
+    console.log(hashInput, "hash")
+    console.log(input, "input")
+}
+
+console.log(letterCounter('amora'))
